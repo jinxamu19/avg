c6c8eeb7c1201c24fd06ef6f9253fc2a8b07be65
jinxamu19 <aamulyasaxena19@gmail.com>
2019-10-30 08:38:25 +0530

    my commit

--- compare numbers.cpp
+++ compare numbers.cpp
@@ -2,17 +2,17 @@
 int main()
 {
 //to compare numbers
-int a,b,c;
+int A,b,c;
 printf("Enter numbers to compare\n");
-scanf("%d %d %d" , &a,&b,&c);
-if (a>b,a>c)
-   printf("The number %d is greater \n" ,a);
+scanf("%d %d %d" , &A,&b,&c);
+if (A>b,A>c)
+   printf("The number %d is greater \n" ,A);
 else if(b>c)
    printf("The number %d is greater \n" ,b);
-else 
+else
    printf("The number %d is greater \n" ,c);
  return 0;
- 
+
 
 
 
